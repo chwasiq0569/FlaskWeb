a69f73a9a09d8b3df4ebfa0edf32d30b2d6aa881
chwasiq0569 <samsunggrandprimeplus056@gmail.com>
2021-04-17 07:12:06 +0500

    Created SQLLite DB

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ class Todo(db.Model):
     sno = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(200), nullable=False)
     desc = db.Column(db.String(500), nullable=False)
-    date_created = db.Column(db.DateTime, default=db.datetime.utcnow)
+    date_created = db.Column(db.DateTime, default=datetime.utcnow)
 
     def __repr__(self):
         return f"{self.sno} - {self.title}"
